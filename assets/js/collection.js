750fa7484690814256c2278ddad512ee8de44d66
shaggy <simmons5family@gmail.com>
2020-11-04 20:55:39 -0600

    Inital price fetch request, game input as string

---
+++ assets/js/collection.js
@@ -0,0 +1,14 @@
+var priceCall = function() {
+    
+var game = "batman";
+
+    fetch('https://www.pricecharting.com/api/products?t=b6347e4a9a79ac34e52eadd448892dfc961d6569&q=' + game)
+    .then(function(response) {
+        return response.json();
+    })
+
+    .then(function(response) {
+        console.log(response);
+    })
+}
+priceCall();
