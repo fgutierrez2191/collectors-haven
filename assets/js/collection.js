18f14325c79dc1859e045fc8f0cda59633bb3926
shaggy <simmons5family@gmail.com>
2020-11-05 18:28:40 -0600

    returns individual games from array

--- assets/js/collection.js
+++ assets/js/collection.js
@@ -1,14 +1,15 @@
 var priceCall = function() {
     
-var game = "spiderman";
-
-    fetch('https://www.pricecharting.com/api/products?t=b6347e4a9a79ac34e52eadd448892dfc961d6569&q=' + game)
-    .then(function(response) {
-        return response.json();
+    var game = ['deathstranding', 'borderlands'];
+    
+    game.forEach(vGame => { 
+        fetch('https://www.pricecharting.com/api/product?t=b6347e4a9a79ac34e52eadd448892dfc961d6569&q=' + vGame)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            console.log(response)
+        })
     })
-
-    .then(function(response) {
-        console.log(response);
-    })
-}
-priceCall();
+    }
+    priceCall();
