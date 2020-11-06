138927ab0b4f2eb2417023e71509f587d473a429
SDM80 <sdawn8912@gmail.com>
2020-11-05 21:39:02 -0600

    added options to carousel on js

--- assets/js/articles.js
+++ assets/js/articles.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.carousel');
+    var options = {
+        fullWidth: true,
+        indicators: true,
+        duration: 3000,
+    };
     var instances = M.Carousel.init(elems, options);
 });
 
-// Or with jQuery
 
-$(document).ready(function () {
-    $('.carousel').carousel();
-});
